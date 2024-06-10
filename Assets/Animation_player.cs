03fd46be3bdb9ec38bf1c9a6764eda81b4dcf0c4
Bram Schouten <459133@student.fontys.nl>
2024-06-10 15:40:26 +0200

    Animations almost done
    Click needs to change to set time, 2/4 seconds

--- Assets/Animation_player.cs
+++ Assets/Animation_player.cs
@@ -6,6 +6,8 @@ public class Animation_player : MonoBehaviour
 {
     public GameObject Camera;
     public GameObject Screen;
+    public GameObject LoadingBar;
+    public GameObject Puzzle;
     private bool started = false;
 
     private AudioSource scannersound;
@@ -39,6 +41,39 @@ public class Animation_player : MonoBehaviour
 
         Animator animatorCamera = Camera.GetComponent<Animator>();
         animatorCamera.SetTrigger("start_camera_movement");
-        
+        Animator animatorScreen = Screen.GetComponent<Animator>();
+        animatorScreen.SetBool("FadeIn", true);
+        StartCoroutine(WaitAndActivateLoadingBar());
+    }
+
+    private IEnumerator WaitAndActivateLoadingBar()
+    {
+        // Wait for 4 seconds
+        yield return new WaitForSeconds(4);
+
+        // Set the LoadingBar animation bool
+        Animator animatorLoadingBar = LoadingBar.GetComponent<Animator>();
+        animatorLoadingBar.SetBool("Active", true);
+        StartCoroutine(WaitAndContinueAnimation());
+    }
+    
+    private IEnumerator WaitAndContinueAnimation()
+    {
+        // Wait for 4 seconds
+        yield return new WaitForSeconds(4);
+
+        Animator animatorScreen = Screen.GetComponent<Animator>();
+        animatorScreen.SetBool("FadeIn", false);
+        animatorScreen.SetBool("FadeOut", true);
+        StartCoroutine(WaitAndLoadPuzzle());
+    }
+    
+    private IEnumerator WaitAndLoadPuzzle()
+    {
+        // Wait for 4 seconds
+        yield return new WaitForSeconds(4);
+
+        Animator animatorPuzzle = Puzzle.GetComponent<Animator>();
+        animatorPuzzle.SetBool("FadeIn", true);
     }
 }
