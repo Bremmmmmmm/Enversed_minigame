8898c013918c0c60a14a67f8d7ffce66b3256833
olde Scheper,Levi L.M.C <l.oldescheper@student.fontys.nl>
2024-06-10 13:56:14 +0200

    small fix

--- Assets/Animation_player.cs
+++ Assets/Animation_player.cs
@@ -12,7 +12,7 @@ public class Animation_player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        scannersound = GetComponent<AudioSourve>();
+        scannersound = GetComponent<AudioSource>();
     }
 
     void Update()
